5c17e82478c36f1c3ac43d410bd564482932217b
Luyanda <luyandasmadonsela@gmail.com>
2024-01-27 20:09:25 +0200

    favicon title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Portfolio Website",
+  title: "Luyanda Madonsela",
   description: "Luyanda Madonsela's Portfolio Website",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
