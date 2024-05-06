58b7a7d8d8324a7954fdd98247361707a0ff482c
Luyanda <luyandasmadonsela@gmail.com>
2024-05-06 21:56:45 +0200

    Nav margin edit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,7 +14,7 @@ const Navigation = () => {
  };
 
  return (
-    <div className="flex  h-28 md:h-32 w-full pr-12 pl-6 md:pl-14 md:pt-6 mb-5 md:mb-10 font-poppins-500 md:text-md lg:text-lg xl:text-xl relative z-50 justify-between md:justify-normal">
+    <div className="flex  h-28 md:h-32 w-full pr-10 pl-6 md:pl-14 md:pt-6 mb-5 md:mb-10 font-poppins-500 md:text-md lg:text-lg xl:text-xl relative z-50 justify-between md:justify-normal">
       <div className='md:mr-10 relative w-32 h-32 md:w-32 md:h-32 lg:w-40 lg:h-40 xl:w-48 xl:h-48'>
         <Link href="/">
           <Image src={logo} layout="fill" objectFit="contain" alt="Personal Logo" />
@@ -40,8 +40,8 @@ const Navigation = () => {
         <Button text="Contact" href="/contact" target='' height="h-10" width="w-28"/>
       </div>
 
-            {/* Hamburger Icon */}
-            <button onClick={toggleMenu} className="md:hidden">
+      {/* Hamburger Icon */}
+      <button onClick={toggleMenu} className="md:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-500">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
