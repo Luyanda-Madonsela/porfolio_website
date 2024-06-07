3bd48f7930e5b05f301fb9494f2ddd83bbac743e
Luyanda Madonsela <88518660+Luyanda-Madonsela@users.noreply.github.com>
2024-06-07 19:17:41 +0000

    Projects & expertise background color changes

--- src/components/ExpertisePlaque.tsx
+++ src/components/ExpertisePlaque.tsx
@@ -10,7 +10,7 @@ interface ExpertisePlaqueProps {
 const ExpertisePlaque: React.FC<ExpertisePlaqueProps> = ({ imageUrl, headingText, paragraphText }) => {
     return (
         <div>
-            <div className='flex flex-col md:w-48 lg:w-48 xl:w-56 bg-custom-gray rounded-b-lg mb-10 md:mb-28'>
+            <div className='flex flex-col md:w-48 lg:w-48 xl:w-56 bg-white rounded-b-lg mb-10 md:mb-28'>
                 <div className='flex h-3 bg-butt-color'></div>
 
                 <div className='flex flex-col mt-8 p-2 md:m-2 lg:m-2 xl:m-4 relative h-36 md:h-44 md:w-44 lg:h-40 lg:w-44 xl:h-56 xl:w-48 justify-center items-center content-center justify-items-center'>
